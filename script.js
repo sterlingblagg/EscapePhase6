b6f3c7e379e59885b844fa0f4bc41e26358b470d
Sterling Blagg <37843170+sterlingblagg@users.noreply.github.com>
2019-02-22 15:18:53 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,59 @@
+function nextImage(el){
+	if (el.src.match("images/symbol1.png")){
+		el.src = "images/symbol2.png";
+	} else if (el.src.match("images/symbol2.png")){
+		el.src = "images/symbol3.png";
+	} else if (el.src.match("images/symbol3.png")){
+		el.src = "images/symbol1.png";
+	} else {
+		
+	}
+}
+
+function nextImage2(el){
+	if (el.src.match("images/symbol1.png")){
+		el.src = "images/symbol2.png";
+	} else if (el.src.match("images/symbol2.png")){
+		el.src = "images/symbol3.png";
+	} else if (el.src.match("images/symbol3.png")){
+		el.src = "images/symbol1.png";
+	} else {
+		
+	}
+}
+
+function nextImage3(el){
+	if (el.src.match("images/symbol1.png")){
+		el.src = "images/symbol2.png";
+	} else if (el.src.match("images/symbol2.png")){
+		el.src = "images/symbol3.png";
+	} else if (el.src.match("images/symbol3.png")){
+		el.src = "images/symbol1.png";
+	} else {
+		
+	}
+}
+
+function nextImage4(el){
+	if (el.src.match("images/symbol1.png")){
+		el.src = "images/symbol2.png";
+	} else if (el.src.match("images/symbol2.png")){
+		el.src = "images/symbol3.png";
+	} else if (el.src.match("images/symbol3.png")){
+		el.src = "images/symbol1.png";
+	} else {
+		
+	}
+}
+
+function nextImage5(el){
+	if (el.src.match("images/symbol1.png")){
+		el.src = "images/symbol2.png";
+	} else if (el.src.match("images/symbol2.png")){
+		el.src = "images/symbol3.png";
+	} else if (el.src.match("images/symbol3.png")){
+		el.src = "images/symbol1.png";
+	} else {
+		
+	}
+}
